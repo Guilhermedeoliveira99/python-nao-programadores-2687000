a518d039fd504e66df10b5c4aacfa18cb17028ee
Guilhermedeoliveira99 <164964607+Guilhermedeoliveira99@users.noreply.github.com>
2024-03-28 00:50:36 +0000

    resolucao 1 lista

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,22 @@
 # Crie uma lista apenas com elementos numéricos
+anos = [2000, 2001, 2003, 2004]
 
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
+pessoas = ['Guilherme', 22, 2002, ['excel', 'PPT', 'Word'], True, ['fut', 'musica', 'assistir'], False]
 
 # Imprima na tela apenas os 5 primeiros elementos da lista
+print(pessoas[:5])
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
+print(pessoas[0:-1:2])
 
 # Remova da lista o último item
+print(pessoas.pop())
 
 # Insira na lista um novo item
+(pessoas.append('28'))
+print(pessoas)
 
 # Remova da lista um item específico
+pessoas.remove(22)
+print(pessoas)
